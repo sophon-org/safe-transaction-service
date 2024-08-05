694a183bce1461b85998bd832d17d0da62c38051
Uxio Fuentefria <uxio0@users.noreply.github.com>
2024-08-05 17:10:17 +0200

    Fix tests

--- safe_transaction_service/safe_messages/apps.py
+++ safe_transaction_service/safe_messages/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class SafeMessagesConfig(AppConfig):
     name = "safe_transaction_service.safe_messages"
     verbose_name = "Safe Messages app"
-
-    def ready(self):
-        from . import signals  # noqa
